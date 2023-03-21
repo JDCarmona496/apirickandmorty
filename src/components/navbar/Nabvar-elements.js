15ddadef1b6ca4b35c7117eb71acea48037b6cb6
David Carmona <jdcarmona496@misena.edu.co>
2023-03-20 22:28:13 -0500

    navbar

--- src/components/navbar/Nabvar-elements.js
+++ src/components/navbar/Nabvar-elements.js
@@ -1,1 +1,36 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+
+export const NavContainer = styled.nav`
+  img {
+    width: auto;
+    height: 70px;
+    margin-left: 5rem;
+    margin-top: .2rem; 
+    margin-bottom: .2rem; 
+  }
+
+  padding .4rem;
+  background-color: #2C352A;
+  display: flex;
+  align-items: center;
+  justify-content:space-between;
+
+  a{
+    color: black;
+    text-decoration: none;
+    margin-right: 1rem;
+    font-family: Marvel;
+    font-size: 25px;
+
+  }
+  .rectangle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 1rem;
+    background-color: green;
+    border-radius:15px;
+    width: 400px;
+    height:40px;
+  }  
+`;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import logo from "../../assets/img/logo.png";
+import "../Navbar/Navbar.css";
+
+import { NavContainer } from "./Nabvar-elements";
 
 const navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <NavContainer>
+        <img src={logo} alt="logo" />
+        <div>
+          <div className="rectangle">
+            <a href="/">Personajes</a>
+            <a href="/">Capitulos</a>
+            <a href="/">Aleatoriedad</a>
+          </div>
+        </div>
+      </NavContainer>
+    </>
+  );
 };
 
 export default navbar;
